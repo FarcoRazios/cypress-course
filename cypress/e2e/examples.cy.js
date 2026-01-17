fe5861655e6f920aed629d198fa13ae7829a604e
Dmitry Yakusevich <collapsefan@gmail.com>
2026-01-17 19:24:33 +0200

    Add data-test attributes for improved testing in GrudgeList component

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -33,10 +33,16 @@ describe('Various examples', () => {
             cy.get('li').should('have.length', 0)
         })
 
+        cy.getDataTest('clear-button').should('not.exist')
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Add Some Grudges')
+
         cy.getDataTest('grudge-input').within(() => {
             cy.get('input').type('some grudge')
         })
         cy.getDataTest('add-grudge-button').click()
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Grudges')
 
         cy.getDataTest('grudge-list').within(() => {
             cy.get('li').should('have.length', 1)
@@ -49,6 +55,19 @@ describe('Various examples', () => {
         cy.getDataTest('grudge-list').within(() => {
             cy.get('li').should('have.length', 2)
             cy.get('li').its(0).should('contains.text', 'some grudge')
+        })
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').its(0).within(() => {
+                cy.get('button').click()
+            })
+            cy.get('li').should('have.length', 1)
+        })
+
+        cy.getDataTest('clear-button').click()
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 0)
         })
     })
 })
